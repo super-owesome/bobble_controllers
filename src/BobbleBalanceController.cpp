24711a6f8d70e89bd3920d402e24aeecb01fbe0b
James <jamesholley22@gmail.com>
2018-10-07 14:17:46 -0500

    just running madgwick once

--- src/BobbleBalanceController.cpp
+++ src/BobbleBalanceController.cpp
@@ -261,11 +261,9 @@ namespace bobble_controllers
 
 
 	void BobbleBalanceController::imuCB(const sensor_msgs::Imu::ConstPtr &imuData) {
-	    for(int i = 0; i < 5; i++) {
-			MadgwickAHRSupdateIMU(imuData->angular_velocity.x, imuData->angular_velocity.y, imuData->angular_velocity.z,
-								  imuData->linear_acceleration.x, imuData->linear_acceleration.y,
-								  imuData->linear_acceleration.z);
-		}
+		MadgwickAHRSupdateIMU(imuData->angular_velocity.x, imuData->angular_velocity.y, imuData->angular_velocity.z,
+							  imuData->linear_acceleration.x, imuData->linear_acceleration.y,
+							  imuData->linear_acceleration.z);
 		tf::Quaternion q(q0, q1, q2, q3);
 		tf::Matrix3x3 m(q);
 		RollDot = imuData->angular_velocity.x;
