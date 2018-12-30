b105831f6c612b7e38fe5da24dcc880c3f1c5023
Mike <mike.moore@so.engineering>
2018-12-30 12:05:02 -0600

    Adding automated tests of the balance controller.

---
+++ test/balance_controller_test.cpp
@@ -0,0 +1,115 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2018, S.O. Engineering, LLC
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of S.O. Engineering, LLC nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//////////////////////////////////////////////////////////////////////////////
+
+/// \author Mike Moore
+
+#include "test_common.h"
+
+// TEST CASES
+TEST_F(BobbleBalanceControllerTest, testBalance)
+{
+  // wait for 3s. Is it holding balance?
+  ros::Duration(3.0).sleep();
+  bobble_controllers::BobbleBotStatus status = getLastStatus();
+  EXPECT_LT(fabs(status.Tilt), 5.0); // we should be standing
+}
+
+TEST_F(BobbleBalanceControllerTest, testForward)
+{
+  // get initial status
+  bobble_controllers::BobbleBotStatus old_status = getLastStatus();
+  // send a forward velocity command of 0.1 m/s
+  geometry_msgs::Twist cmd_vel;
+  cmd_vel.linear.x = 0.1;
+  publish_vel_cmd(cmd_vel);
+  // wait for 8s
+  ros::Duration(8.0).sleep();
+  bobble_controllers::BobbleBotStatus new_status = getLastStatus();
+  const double expected_velocity = 0.1;
+  const double dvx = new_status.ForwardVelocity - old_status.ForwardVelocity;
+  EXPECT_NEAR(dvx, expected_velocity, VELOCITY_TOLERANCE);
+}
+
+TEST_F(BobbleBalanceControllerTest, testBackward)
+{
+  // get initial status
+  bobble_controllers::BobbleBotStatus old_status = getLastStatus();
+  // send a forward velocity command of -0.1 m/s
+  geometry_msgs::Twist cmd_vel;
+  cmd_vel.linear.x = -0.1;
+  publish_vel_cmd(cmd_vel);
+  // wait for 8s
+  ros::Duration(8.0).sleep();
+  bobble_controllers::BobbleBotStatus new_status = getLastStatus();
+  const double expected_velocity = -0.1;
+  const double dvx = new_status.ForwardVelocity - old_status.ForwardVelocity;
+  EXPECT_NEAR(dvx, expected_velocity, VELOCITY_TOLERANCE);
+}
+
+TEST_F(BobbleBalanceControllerTest, testTurnLeft)
+{
+  // get initial status
+  bobble_controllers::BobbleBotStatus old_status = getLastStatus();
+  // send a turn rate command of 0.1 rad/s
+  geometry_msgs::Twist cmd_vel;
+  cmd_vel.angular.z = 0.1;
+  publish_vel_cmd(cmd_vel);
+  // wait for 3s
+  ros::Duration(3.0).sleep();
+  bobble_controllers::BobbleBotStatus new_status = getLastStatus();
+  const double expected_turn_rate = 25.0;
+  EXPECT_GT(new_status.TurnRate, expected_turn_rate); // should be turning at least 25 deg/s
+}
+
+TEST_F(BobbleBalanceControllerTest, testTurnRight)
+{
+  // get initial status
+  bobble_controllers::BobbleBotStatus old_status = getLastStatus();
+  // send a turn rate command of -0.1 rad/s
+  geometry_msgs::Twist cmd_vel;
+  cmd_vel.angular.z = -0.1;
+  publish_vel_cmd(cmd_vel);
+  // wait for 3s
+  ros::Duration(3.0).sleep();
+  bobble_controllers::BobbleBotStatus new_status = getLastStatus();
+  const double expected_turn_rate = -25.0;
+  EXPECT_LT(new_status.TurnRate, expected_turn_rate); // should be turning at least -25 deg/s
+}
+
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "balance_controller_test");
+
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  int ret = RUN_ALL_TESTS();
+  spinner.stop();
+  ros::shutdown();
+  return ret;
+}
---
+++ test/test_common.h
@@ -0,0 +1,142 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2018, S.O. Engineering, LLC
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of S.O. Engineering, LLC nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//////////////////////////////////////////////////////////////////////////////
+
+/// \author Mike Moore
+
+#include <cmath>
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <bobble_controllers/ControlCommands.h>
+#include <bobble_controllers/BobbleBotStatus.h>
+
+// Floating-point value comparison threshold
+const double VELOCITY_TOLERANCE = 0.05; // 5 cm-s precision
+
+class BobbleBalanceControllerTest : public ::testing::Test
+{
+public:
+
+  BobbleBalanceControllerTest()
+  : mode_cmd_pub(nh.advertise<bobble_controllers::ControlCommands>("/bobble/bobble_balance_controller/bb_cmd", 1)),
+    cmd_vel_pub(nh.advertise<geometry_msgs::Twist>("/bobble/bobble_balance_controller/cmd_vel", 1)),
+    bobble_status_sub(nh.subscribe("/bobble/bobble_balance_controller/bb_controller_status", 1, &BobbleBalanceControllerTest::statusCallback, this))
+  {
+  }
+
+  ~BobbleBalanceControllerTest()
+  {
+    bobble_status_sub.shutdown();
+  }
+
+  void SetUp(){
+    waitForController();
+    ros::Duration(0.25).sleep();
+    reset();
+    ROS_INFO("Activating balance controller.");
+    ros::Duration(0.25).sleep();
+    activate();
+  }
+
+  void publish_vel_cmd(geometry_msgs::Twist cmd_vel){ cmd_vel_pub.publish(cmd_vel); }
+
+  bobble_controllers::BobbleBotStatus getLastStatus(){ return last_status; }
+
+  void activate()
+  {
+    bobble_controllers::ControlCommands mode_cmd;
+    mode_cmd.StartupCmd = true;
+    mode_cmd.IdleCmd = false;
+    mode_cmd.DiagnosticCmd = false;
+    mode_cmd_pub.publish(mode_cmd);
+  }
+
+  void reset()
+  {
+    geometry_msgs::Twist cmd_vel;
+    cmd_vel.linear.x = 0.0;
+    cmd_vel.angular.z = 0.0;
+    publish_vel_cmd(cmd_vel);
+    ros::Duration(0.25).sleep();
+    bobble_controllers::ControlCommands mode_cmd;
+    mode_cmd.IdleCmd = true;
+    mode_cmd.StartupCmd = false;
+    mode_cmd.DiagnosticCmd = false;
+    mode_cmd_pub.publish(mode_cmd);
+    ros::Duration(0.25).sleep();
+  }
+
+  bool isControllerAlive()const{ return (bobble_status_sub.getNumPublishers() > 0) && (cmd_vel_pub.getNumSubscribers() > 0); }
+
+  void waitForController() const
+  {
+    while(!isControllerAlive() && ros::ok())
+    {
+      ROS_DEBUG_STREAM_THROTTLE(0.5, "Waiting for controller.");
+      ros::Duration(0.1).sleep();
+    }
+    if (!ros::ok())
+      FAIL() << "Something went wrong while executing test.";
+  }
+
+  void statusCallback(const bobble_controllers::BobbleBotStatus& status)
+  {
+    ROS_INFO_STREAM("Callback received: status.ForwardVelocity: " << status.ForwardVelocity
+                     << ", status.Tilt: " << status.Tilt);
+    last_status = status;
+    received_first_status = true;
+  }
+
+  bool hasReceivedFirstStatus()const{ return received_first_status; }
+
+  void waitForStatusMsgs() const
+  {
+    while(!hasReceivedFirstStatus() && ros::ok())
+    {
+      ROS_DEBUG_STREAM_THROTTLE(0.5, "Waiting for status messages to be published.");
+      ros::Duration(0.01).sleep();
+    }
+    if (!ros::ok())
+      FAIL() << "Something went wrong while executing test.";
+  }
+
+  double angular_distance(double alpha, double beta) const
+  {
+      double phi = fmod(fabs(beta - alpha), 360);       // This is either the distance or 360 - distance
+      double distance = phi > 180.0 ? 360.0 - phi : phi;
+      return distance;
+  }
+
+private:
+  bool received_first_status;
+  ros::NodeHandle nh;
+  ros::Publisher mode_cmd_pub;
+  ros::Publisher cmd_vel_pub;
+  ros::Subscriber bobble_status_sub;
+  bobble_controllers::BobbleBotStatus last_status;
+};
+
